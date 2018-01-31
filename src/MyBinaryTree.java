514569dd0482904ce633edf21b35c32df7416cff
Marion Schleifer <marion.schleifer@simplificator.com>
2018-01-31 15:43:48 +0100

    attach subtrees

--- src/MyBinaryTree.java
+++ src/MyBinaryTree.java
@@ -70,4 +70,22 @@ public class MyBinaryTree<E> {
         return tmp;
     }
 
+    public void attach(MyTreeNode node, MyBinaryTree tree1, MyBinaryTree tree2) throws IllegalArgumentException {
+        if (node.getLeftChild() != null || node.getRightChild() != null) {
+            throw new IllegalArgumentException("This node already has one or more childern");
+        }
+        size += tree1.size += tree2.size;
+        if (!tree1.isEmpty()) {
+            root.setParent(node);
+            node.setLeftChild(tree1.root);
+            tree1.root = null;
+            tree1.size = 0;
+        }
+        if (!tree2.isEmpty()) {
+            root.setParent(node);
+            node.setRightChild(tree2.root);
+            tree2.root = null;
+            tree2.size = 0;
+        }
+    }
 }
--- test/MyBinaryTreeTest.java
+++ test/MyBinaryTreeTest.java
@@ -32,4 +32,30 @@ public class MyBinaryTreeTest {
         assertEquals(21, tree.root.getRightChild().getValue());
         assertEquals(2, tree.getSize());
     }
+
+    @Test
+    void setValue() {
+        MyBinaryTree tree = new MyBinaryTree();
+        tree.setRoot(42);
+        tree.root.setValue(31);
+        assertEquals(31, tree.root.getValue());
+    }
+
+    @Test
+    void attach() {
+        MyBinaryTree tree = new MyBinaryTree();
+        tree.setRoot(1);
+        MyBinaryTree subTree1 = new MyBinaryTree();
+        subTree1.setRoot(2);
+        MyBinaryTree subTree2 = new MyBinaryTree();
+        subTree2.setRoot(3);
+        tree.attach(tree.root, subTree1, subTree2);
+        assertEquals(2, tree.root.getLeftChild().getValue());
+        assertEquals(3, tree.root.getRightChild().getValue());
+        assertEquals(null, subTree1.root);
+        assertEquals(null, subTree2.root);
+        assertEquals(3, tree.getSize());
+        assertEquals(0, subTree1.getSize());
+        assertEquals(0, subTree2.getSize());
+    }
 }
